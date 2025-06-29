e3b0b342c930ffe622fbe91d1af94d6140d438f8
Tanner Cox <tanner@wavesandwebs.com>
2025-06-29 16:36:20 -0700

    need to run seeder

--- packages/Webkul/Installer/src/Console/Commands/Installer.php
+++ packages/Webkul/Installer/src/Console/Commands/Installer.php
@@ -297,12 +297,16 @@ class Installer extends Command
      */
     protected function askForDatabaseDetails()
     {
+        $databaseConnection = select(
+            label   : 'Please select the database connection',
+            options : ['mysql', 'pgsql'],
+            default : 'mysql',
+        );
+
+        $defaultPort = $databaseConnection === 'pgsql' ? '5432' : '3306';
+
         $databaseDetails = [
-            'DB_CONNECTION' => select(
-                label   : 'Please select the database connection',
-                options : ['mysql'],
-                default : 'mysql',
-            ),
+            'DB_CONNECTION' => $databaseConnection,
 
             'DB_HOST' => text(
                 label    : 'Please enter the database host',
@@ -312,7 +316,7 @@ class Installer extends Command
 
             'DB_PORT' => text(
                 label    : 'Please enter the database port',
-                default  : $this->getEnvVariable('DB_PORT', '3306'),
+                default  : $this->getEnvVariable('DB_PORT', $defaultPort),
                 required : true
             ),
 
